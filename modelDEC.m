48f0fe3660393846c7015086ec879c219ca1f5b8
jtdimasaka <jtd33@cam.ac.uk>
2024-09-11 20:27:52 +0200

    Working code for single rID implementation

--- modelDEC.m
+++ modelDEC.m
@@ -22,6 +22,7 @@ label2rasterID = readtable("data/MASK/MAPPING_RASTER_ID_AND_LABEL.xlsx");
 [bldgftprnt_ove, ~] = readgeoraster("data/BLDG/OVERTURE/rasterized_Overture.tif");
 [bldgftprnt_goo, ~] = readgeoraster("data/BLDG/GOOGLE 042021-052023/rasterized_googleOpenBldg.tif");
 [bldgftprnt_mic, ~] = readgeoraster("data/BLDG/MICROSOFT 2014-2021/rasterized_microsoftbldg.tif");
+[btype_label, ~] = readgeoraster("data/BLDG/BACHOFER DLR/EO4Kigali_2015_btype.tif");
 bldgftprnt = (bldgftprnt_osm > 0) | (bldgftprnt_ove > 0) | (bldgftprnt_goo > 0) | (bldgftprnt_mic > 0);
 % geotiffwrite("data/BLDG/bldgcombined.tif",single(bldgftprnt),maskR)
 
@@ -47,13 +48,15 @@ y_wall = zeros(size(mask));
 % rID = 403, Sanity Check 1: Case of Gatenga (sector), City of Kigali
 % rID = 412, Sanity Check 2: Case of Gitega (sector), City of Kigali
 % rID = 404, Sanity Check 3: Case of Gikondon
+% rID = 411, DEC Check 1: Case of 'Nyarugunga'
+% rID = 414, DEC Check 1: Case of 'Kigali'
 % subset
 sub_label2rasterID = readtable("data/BLDG/BACHOFER DLR/rID_coverage.csv");
 
 % for rID = 1:length(label2rasterID.RASTER_ID1)
-for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
+for idx_rID = 1:1 %1:length(sub_label2rasterID.RASTER_ID1)
 
-    rID = sub_label2rasterID.RASTER_ID1(idx_rID);
+    rID = 411; % sub_label2rasterID.RASTER_ID1(idx_rID);
 
     disp("start"), tic
 
@@ -234,6 +237,7 @@ for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
         x_swir1     = sparse(double(swir1.*valid_idx));
         x_swir2     = sparse(double(swir2.*valid_idx));
         x_nir       = sparse(double(nir.*valid_idx));
+        sub_label_roof  = sparse(double(btype_label.*valid_idx));
         toc, disp("Covariate X Prepared"), tic
         %% ground truth class representation for global clustering
         % ROOF - common to all rIDs
@@ -294,6 +298,7 @@ for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
         % relationship between roof material, wall material, and height class.
         n_class = sum(summary_RoofMaterial > 1e-5);
         [row,col] = find(x_s1vv>0); % incorporate spatial element
+        ind = find(x_s1vv>0);
         X = full([  ...
                     normalize(log(x_s1vv(x_s1vv>0))) ...
                     normalize(log(x_s1vh(x_s1vh>0))) ...
@@ -314,7 +319,7 @@ for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
         
         %% DR
 
-        % % PCA - linear, can be probabilistic using PPCA
+        % PCA - linear, can be probabilistic using PPCA
         % [~,score,~,~,explainedVar] = pca(X);
         % sum_explainedVar = 0;
         % for i = 1:length(explainedVar)
@@ -328,46 +333,174 @@ for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
         % [Y,loss] = tsne(X, "Distance","euclidean");
         % X = Y;
 
-        % autoencoder - deep & nonlinear, probabilistic via variational,
+        % tau = floor(summary_RoofMaterial .* size(X,1));
+        % if sum(tau) ~= size(X,1)
+        %     ttmp = find(tau == max(tau),1);
+        %     tau(ttmp) = tau(ttmp) + (size(X,1)-sum(tau));
+        % end
+        % err = 1; failed = 1;
+        % while err > 0.01 | failed == 1
+        %     try
+        %         [labels,centroids] = constrainedKMeans(X, n_class, tau(tau ~= 0), 1000);
+        %         failed = 0;
+        %     catch MyErr
+        %         failed = 1;
+        %     end
+        %     if failed == 0
+        %         err = sum(abs((tau(tau ~= 0))'-(histcounts(labels)))) ./ sum(tau(tau ~= 0));
+        %     end
+        % end
+
+
+        %% autoencoder - deep & nonlinear, probabilistic via variational,
         % image-applicable via convolutional, 
-        hiddenSize = 5;
-        autoenc = trainAutoencoder(X',hiddenSize);
-        Z = encode(autoenc,X');
-        X = Z';
 
-        %% Constrained K-Means clustering algorithm
-        % Bradley, P. S., Bennett, K. P., & Demiriz, A. (2000). 
-        % Constrained k-means clustering. Microsoft Research, Redmond, 20(0), 0.
-        % https://uk.mathworks.com/matlabcentral/fileexchange/117355-constrained-k-means
-        tau = floor(summary_RoofMaterial .* size(X,1));
+        numFeatures = size(X,2);
+        hiddenSize = 8;
+        numLatentChannels = 1;
 
-        % we gotta remove this because this results in an ill-posed
-        % solution for clustering roof material
-        if sum(tau) ~= size(X,1)
-            ttmp = find(tau == max(tau),1);
-            tau(ttmp) = tau(ttmp) + (size(X,1)-sum(tau));
+        % layersE = [
+        %     featureInputLayer(numFeatures) %12
+        %     fullyConnectedLayer(hiddenSize) %10
+        %     layerNormalizationLayer %10
+        %     reluLayer %10
+        %     fullyConnectedLayer(2*numLatentChannels) %10
+        %     samplingLayer]; %5
+
+        layersE = [
+            featureInputLayer(numFeatures) %12
+            fullyConnectedLayer(hiddenSize) %10
+            layerNormalizationLayer %10
+            reluLayer %10
+            fullyConnectedLayer(numLatentChannels)
+            sigmoidLayer]; %5
+
+        layersD = [
+            featureInputLayer(numLatentChannels) %5
+            fullyConnectedLayer(hiddenSize) %10
+            reluLayer %10
+            fullyConnectedLayer(12)  %12
+            reluLayer %12
+            fullyConnectedLayer(numFeatures)];
+
+
+        netE = dlnetwork(layersE);
+        netD = dlnetwork(layersD);
+
+        numEpochs = 500;
+        learnRate = 1e-3;
+        regularization = 0.05;
+
+        trailingAvgE = [];
+        trailingAvgSqE = [];
+        trailingAvgD = [];
+        trailingAvgSqD = [];
+
+        % applicable only for minibatch setting
+        % numObservationsTrain = size(X,1);
+        % miniBatchSize = 1; 
+        % numIterationsPerEpoch = ceil(numObservationsTrain / miniBatchSize);
+        % numIterations = numEpochs * numIterationsPerEpoch;
+
+        monitor = trainingProgressMonitor;
+        % monitor.Info = ["ClusteringLoss","VariationalAndReconstructionLoss","PredictionLoss","Epoch"];
+        % monitor.Metrics = ["ClusteringLoss","VariationalAndReconstructionLoss","PredictionLoss"];
+        monitor.Info = ["ReconstructionLoss","PredictionLoss","GlobalLocalLoss","Epoch"];
+        monitor.Metrics = ["ReconstructionLoss","PredictionLoss","GlobalLocalLoss",...
+            "TPprop","GLConsistency","Precision","Recall","Accuracy","F1Score"];
+        monitor.XLabel = "Epoch";
+        % groupSubPlot(monitor,"ClusteringLoss","ClusteringLoss");
+        groupSubPlot(monitor,"ReconstructionLoss","ReconstructionLoss");
+        groupSubPlot(monitor,"PredictionLoss","PredictionLoss");
+        groupSubPlot(monitor,"GlobalLocalLoss","GlobalLocalLoss");
+        groupSubPlot(monitor,"TPprop","TPprop");
+        groupSubPlot(monitor,"GLConsistency","GLConsistency");
+        groupSubPlot(monitor,"Precision","Precision");
+        groupSubPlot(monitor,"Recall","Recall");
+        groupSubPlot(monitor,"Accuracy","Accuracy");
+        groupSubPlot(monitor,"F1Score","F1Score");
+        X = dlarray(X, 'BC');
+
+        % Loop over epochs.
+        epoch = 0;  
+        while epoch < numEpochs && ~monitor.Stop
+            epoch = epoch + 1
+
+            % Evaluate loss and gradients.
+            if epoch == 1
+                loss2_prev = 1;
+                loss3_prev = 1;
+                loss4_prev = 1;
+            end
+            [loss2,loss3,loss4,...
+                xTPprop,xPre,xRec,xAccu,xF1,consistencyLocalGlobal,...
+                gradientsE,gradientsD] = ...
+                dlfeval(@modelLoss, netE,netD,X, ...
+                        summary_RoofMaterial,n_class,sub_label_roof,ind, ...
+                        loss2_prev, loss3_prev, loss4_prev);
+            loss2_prev = loss2;
+            loss3_prev = loss3;
+            loss4_prev = loss4;
+
+            % Update learnable parameters.
+            [netE,trailingAvgE,trailingAvgSqE] = adamupdate(netE, ...
+                gradientsE,trailingAvgE,trailingAvgSqE,epoch,learnRate);
+
+            [netD, trailingAvgD, trailingAvgSqD] = adamupdate(netD, ...
+                gradientsD,trailingAvgD,trailingAvgSqD,epoch,learnRate);
+
+            [netD, trailingAvgD, trailingAvgSqD] = adamupdate(netD, ...
+                gradientsD,trailingAvgD,trailingAvgSqD,epoch,learnRate);
+
+            % Update the training progress monitor. 
+            % recordMetrics(monitor,epoch, ...
+            % ClusteringLoss=loss1, ...
+            % VariationalAndReconstructionLoss=loss2, ...
+            % PredictionLoss=loss3);
+            % recordMetrics(monitor,epoch, ...
+            % VariationalAndReconstructionLoss=loss2, ...
+            % PredictionLoss=loss3, ...
+            % TPprop = xTPprop, ...
+            % Precision = xPre, ...
+            % Recall = xRec, ...
+            % Accuracy = xAccu, ...
+            % F1Score = xF1);
+            recordMetrics(monitor,epoch, ...
+            ReconstructionLoss=loss2, ...
+            PredictionLoss=loss3, ...
+            GlobalLocalLoss=loss4, ...
+            TPprop=xTPprop, ...
+            GLConsistency=consistencyLocalGlobal, ...
+            Precision = xPre, ...
+            Recall = xRec, ...
+            Accuracy = xAccu, ...
+            F1Score = xF1);
         end
 
-        % try
-        %     rng(1,"v5normal");
-        %     [labels,centroids] = constrainedKMeans(X, n_class, tau(tau ~= 0), 10);
-        % catch MyErr
-        %     [labels,centroids] = constrainedKMeans(X, n_class, tau(tau ~= 0), 10);
-        % end
-        % [~,score,~,~,explainedVar] = pca(X);
+        % Predict
+        [Z,mu,logSigmaSq] = forward(netE,X);
+        % Perform clustering.
+        tau = floor(summary_RoofMaterial .* size(Z,2));
+        if sum(tau) ~= size(Z,2)
+            ttmp = find(tau == max(tau),1);
+            tau(ttmp) = tau(ttmp) + (size(Z,2)-sum(tau));
+        end
         err = 1; failed = 1;
         while err > 0.01 | failed == 1
             try
-                [labels,centroids] = constrainedKMeans(X, n_class, tau(tau ~= 0), 1000);
+                [labels,centroids] = constrainedKMeans(double(extractdata(Z)'), ...
+                    n_class, tau(tau ~= 0), 1000);
                 failed = 0;
             catch MyErr
                 failed = 1;
             end
             if failed == 0
-                err = sum(abs((tau(tau ~= 0))'-(histcounts(labels)))) ./ sum(tau(tau ~= 0));
+                err = sum(abs((tau(tau ~= 0))'-(histcounts(labels)))) ...
+                    ./ sum(tau(tau ~= 0));
             end
         end
-        
+
+       
         % assign roof material
         roof_assignment = strings(size(X,1),1);
         nonzero_idx_tau = find(tau ~= 0);
@@ -626,7 +759,7 @@ for idx_rID = 1:length(sub_label2rasterID.RASTER_ID1)
 end
 
 %%
-geotiffwrite("output/20240829_DR_AE/y_height.tif",(y_height),maskR)
-geotiffwrite("output/20240829_DR_AE/y_roof.tif",(y_roof),maskR)
-geotiffwrite("output/20240829_DR_AE/y_macrotaxo.tif",(y_macrotaxo),maskR)
-geotiffwrite("output/20240829_DR_AE/y_wall.tif",(y_wall),maskR)
+geotiffwrite("output/20240907_DC_411/y_height.tif",(y_height),maskR)
+geotiffwrite("output/20240907_DC_411/y_roof_DC.tif",(y_roof),maskR)
+geotiffwrite("output/20240907_DC_411/y_macrotaxo.tif",(y_macrotaxo),maskR)
+geotiffwrite("output/20240907_DC_411/y_wall.tif",(y_wall),maskR)
